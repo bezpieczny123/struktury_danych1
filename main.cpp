e061d18e4ba4c88f737e5504710fa464a604c2da
bezpieczny123 <m.sadowski.2004@gmail.com>
2025-04-13 14:33:30 +0200

    time measure and linked list improvements

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <filesystem>
 
 #include "slinked_list.hpp"
-//#include "dynamic_arraay.hpp"
+#include "dynamic_array.hpp"
 
 std::ofstream file;
 
@@ -20,44 +20,48 @@ void stopTimer() {
     file << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << ",";
 }
 
-#define MEASURE_SOME(function) \
+#define MEASURE_SOME(function_call) \
     startTimer(); \
-    function; \
+    function_call; \
     stopTimer();
 
-#define MEASURE_ALL(structure) \
-    file.open(#structure ".csv"); \
-    file << #structure << "\n"; \
-    file << "addFront, removeFront, addBack, removeBack, addChosen, removeChosen, findIndexOf\n"; \
-    structure<int> List; \
-    begin = std::chrono::steady_clock::now(); \
-    end = std::chrono::steady_clock::now(); \
-    \
-    std::vector<int> listSizes = {0, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000}; \
-    for (int a = 1; a < 12; a++) {\
-        int n = listSizes[a]; \
-        for(int i = 1; i < listSizes[a] - listSizes[a - 1]; i++) {\
-            List.addFront(156); \
-        } \
-        for(int i = 0; i < 100; i++) { \
-            file << n << ","; \
-            MEASURE_SOME(List.addFront(35)); \
-            MEASURE_SOME(List.removeFront()); \
-            MEASURE_SOME(List.addBack(35)); \
-            MEASURE_SOME(List.removeBack()); \
-            MEASURE_SOME(List.addChosen(7, n / 2)); \
-            MEASURE_SOME(List.removeChosen(n / 2)); \
-            MEASURE_SOME(List.findIndexOf(n / 2)); \
-            file << "\n"; \
-            std::cout << std::endl; \
-        } \
-        file << "\n"; \
-    }\
-    file.close();
 
+template <template <typename> class StructureType>
+void measure_structure(const std::string& structureName, const std::vector<int>& listSizes) {
+    file.open(structureName + ".csv");
+    file << structureName << "\n";
+    file << "addFront, removeFront, addBack, removeBack, addChosen, removeChosen, findIndexOf\n"; \
+    StructureType<int> List;
+    for (int a = 1; a < 12; a++) {
+        int n = listSizes[a];
+        for(int i = 1; i < listSizes[a] - listSizes[a - 1]; i++) {
+            if (structureName == "SLinkedList") {
+                List.addFront(23);
+            } else {
+                List.addBack(23);
+            }
+        }
+        for(int i = 0; i < 100; i++) {
+            file << n << ",";
+            MEASURE_SOME(List.addFront(35));
+            MEASURE_SOME(List.removeFront());
+            MEASURE_SOME(List.addBack(35));
+            MEASURE_SOME(List.removeBack());
+            MEASURE_SOME(List.addChosen(7, n / 2));
+            MEASURE_SOME(List.removeChosen(n / 2));
+            MEASURE_SOME(List.findIndexOf(n / 2));
+            file << "\n";
+            std::cout << std::endl;
+        }
+        file << "\n";
+    }
+    file.close();
+}
 
 int main() {
-    MEASURE_ALL(SLinkedList);
+    std::vector<int> listSizes = {0, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000, 1024000};
+    measure_structure<SLinkedList>("SLinkedList", listSizes);
+    measure_structure<DynamicArray>("DynamicArray", listSizes);
 
     return EXIT_SUCCESS;
 }
